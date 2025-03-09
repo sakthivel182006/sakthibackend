b1769dc41b3e73ad139bcdfa6e19554a7cc7ca25
sakthivel182006 <sakthivelv202222@gmail.com>
2025-03-09 22:18:05 +0530

    backkend path update

--- src/store/store.userBooking.js
+++ src/store/store.userBooking.js
@@ -1,7 +1,7 @@
 // store.userBooking.js
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/users/book"; // Adjusted to match your backend route
+const API_URL = "https://sivanethunaibackend.onrender.com/api/users/book"; // Adjusted to match your backend route
 
 const userBooking = {
   registerUser: async (formData) => {
